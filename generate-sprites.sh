d9224e54db50fc83650695dfca424abeca94a819
anilkunchalaece <anilkunchalaece@gmail.com>
2019-05-28 18:06:44 +0530

    sprites are working

--- generate-sprites.sh
+++ generate-sprites.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 makiPath=node_modules/@mapbox/maki/icons/
-shieldIconsPath=shield_icons/
+# shieldIconsPath=shield_icons/
 outputDir=sprites
 spriteZeroPath=node_modules/@mapbox/spritezero-cli/bin/spritezero
 iconConfigPath=src/styles/
@@ -8,8 +8,8 @@ iconConfigPath=src/styles/
 mkdir temp
 mkdir ${outputDir}
 
-source ~/.bash_profile
-nvm use --delete-prefix v8.9.4
+#source ~/.bash_profile
+#nvm use --delete-prefix v8.9.4
 
 colorize () {
     iconName=$1
@@ -35,19 +35,19 @@ colorize () {
 
 createIconSet () {
 	cp -R ${makiPath} temp/$2
-	cp -R ${shieldIconsPath} temp/$2
+	# cp -R ${shieldIconsPath} temp/$2
 
 	while IFS=, read -r col1 col2 col3
 	do
 		colorize $col1 $col2 $2 $col3
 	done < $1
-
+    cat ${outputDir}/$2
 	${spriteZeroPath} ${outputDir}/$2 temp/$2 --ratio=2
 	${spriteZeroPath} ${outputDir}/$2@2x temp/$2  --ratio=4
 }
 
 createIconSet ${iconConfigPath}apollo-bright-icons.csv apollo-bright-icons
-createIconSet ${iconConfigPath}zen-icons.csv zen-icons
-createIconSet ${iconConfigPath}toner-icons.csv toner-icons
+# createIconSet ${iconConfigPath}zen-icons.csv zen-icons
+# createIconSet ${iconConfigPath}toner-icons.csv toner-icons
 
 rm -rf temp
